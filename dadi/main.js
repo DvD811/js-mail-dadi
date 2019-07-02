60bdb9dd234526ef07ec11a7ab1a5e2a9e1e7b49
DvD811 <davide.gallizioli@gmail.com>
2019-07-02 18:43:38 +0200

    cose

--- dadi/main.js
+++ dadi/main.js
@@ -9,11 +9,12 @@ var p2_dado2 = Math.floor(Math.random() * 6);
 
 
 function singRis() {
-    document.getElementById("ris_p1").innerHTML = "Risultato del primo dado: " + p1_dado1 + "<br>" +
-        "Risultato del secondo dado: " + p1_dado2 + "<br> Risultato finale: " + (p1_dado1 + p1_dado2);
+    document.getElementById("ris_p1").innerHTML = "Risultato del primo dado:<br> " + p1_dado1 + "<br>" +
+        "Risultato del secondo dado:<br> " + p1_dado2 + "<br> Risultato finale:<br> " + (p1_dado1 + p1_dado2);
 };
 
 function singRis2() {
-    document.getElementById("ris_p2").innerHTML = "Risultato del primo dado: " + p2_dado1 + "<br>" +
-        "Risultato del secondo dado: " + p2_dado2 + "<br> Risultato finale: " + (p2_dado1 + p2_dado2);
+    document.getElementById("ris_p2").innerHTML = "Risultato del primo dado:<br> " + p2_dado1 + "<br>" +
+        "Risultato del secondo dado:<br> " + p2_dado2 + "<br> Risultato finale:<br> " +
+        (p2_dado1 + p2_dado2);
 };
